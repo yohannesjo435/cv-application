1c30804f657c7eabed201b11366cefbd5ff8e5f0
Yohannes Tesfaye <yohannes435@gmaill.com>
2025-02-14 13:28:32 -0900

    added deploy.yml, fixed basic style, changed demoInfos add base in vit.config.js and add deploy script in package.json and installed github pages

--- src/demoInformation.js
+++ src/demoInformation.js
@@ -5,27 +5,35 @@ const demoExample = {
     fullName: "Yohannes Tesfaye",
     email: "gofereTech@gmail.com",
     phone: "0901020304",
-    address: "Addis Ababa, Ethiopia"
+    address: "Addis Ababa, Ethiopia",
   },
-  educationalDetails: [{
-    school: "University of Gondar",
-    degree: "Computer Science",
-    startDate: "01/02/1998",
-    endDate: "10/10/2024",
-    location: "Gondar, Ethiopia"
-  }],
+  educationalDetails: [
+    {
+      school: "University of Gondar",
+      degree: "Computer Science",
+      startDate: "01/02/2020",
+      endDate: "10/10/2024",
+      location: "Gondar, Ethiopia",
+    },
+    {
+      school: "Addis Ababa University",
+      degree: "Management",
+      startDate: "01/02/2018",
+      endDate: "10/10/2022",
+      location: "Addis Ababa, Ethiopia",
+    },
+  ],
   experienceDetails: [
     {
       companyName: "GOFERE ENTERPRISE",
       positionTitle: "Co-CEO",
-      startDate: '2001/01/02',      
-      endDate: '2020/12/31',
+      startDate: "2001/01/02",
+      endDate: "2020/12/31",
       location: "Addis Ababa, Ethiopia",
-      description: "Mannage on accessibility standards for the open web. Created and usability tested wireframes and prototypes. Produced interactive documentation for quick onboarding of new researchers.???"
-    }
-  ]
-
-
-}
+      description:
+        "Led strategic initiatives and business development, Oversaw operations and managed a diverse team.",
+    },
+  ],
+};
 
 export default demoExample;
